dbe9de4010382860e498124668958961a9552cb0
Anuhk <145833081+Anuhk@users.noreply.github.com>
2026-01-15 17:15:23 +0530

    Added the XML and logic required for Greetings App, it is working

--- app/src/main/java/com/practice/greetingsapp/MainActivity.java
+++ app/src/main/java/com/practice/greetingsapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.practice.greetingsapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,34 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView greetText;
+    EditText username;
+    Button greetBtn;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        greetText=findViewById(R.id.greetText);
+        username=findViewById(R.id.username);
+        greetBtn=findViewById(R.id.greetBtn);
+
+        greetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name=username.getText().toString().trim();
+                if(!name.isEmpty()){
+                    greetText.setText("Namaste, "+name);
+                }
+                else{
+                    greetText.setText("Namaste");
+                }
+            }
         });
+
+
+
+
     }
 }
